789881b44091ca5c1b0bb8ecac3156a68d01d798
Saharsh joshi <23csa3bcc1026@meu.edu.in>
2026-03-15 16:21:28 +0530

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "domain_name" {
+  description = "Domain name for the portfolio site"
+  default     = "my-portfolio-xyz.com"
+}
